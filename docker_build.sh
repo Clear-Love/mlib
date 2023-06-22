a7407ebf06ed85aef8cb19af5dbbeeb48de9edc6
lmio <2091319361@qq.com>
2023-06-23 00:27:07 +0800

    fix: docker

--- docker_build.sh
+++ docker_build.sh
@@ -4,9 +4,10 @@
 cd mlib-backend || exit
 docker compose up -d
 
+
+cd ../ || exit
 # 构建镜像
 docker build -t mlib-ui ./mlib-ui
 
-cd ../ || exit
 # 运行容器
 docker run -d --name mlib-ui -p 80:80 mlib-ui
